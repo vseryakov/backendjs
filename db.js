f9815c6f11cfaea0d72eae0f03e0c8fdbc5fc529
Vlad Seryakov <vseryakov@gmail.com>
2015-05-03 12:42:10 -0400

    use mtime to pull only updated recods from the config during polls

--- db.js
+++ db.js
@@ -339,6 +339,11 @@ db.initPool = function(name, options, callback)
 //  - current instance tag or a custom tag for ad-hoc queries: `nat`
 //  - current instance IP address: `192.168.1.1`
 //
+// The options takes the following properties:
+//  - force - if true then force to refresh and reopen all db pools
+//  - refresh - if true then pull all config options, otherwise pull only records updated since the time the last config pull, it uses the max mtime
+//     from all received records.
+//
 // On return, the callback second argument will receive all parameters received form the database as a list: -name value ...
 db.initConfig = function(options, callback)
 {
@@ -372,10 +377,11 @@ db.initConfig = function(options, callback)
     });
     // Make sure we have only unique items in the list, skip empty or incomplete items
     types = lib.strSplitUnique(types);
+    if (!this._configMtime || options.force || options.refresh) this._configMtime = 0;
 
-    logger.debug("intConfig:", core.role, self.config, types);
+    logger.debug("intConfig:", core.role, this.config, types, this._configMtime);
 
-    self.select(options.table || "bk_config", { type: types }, { ops: { type: "in" }, pool: self.config }, function(err, rows) {
+    self.select(options.table || "bk_config", { type: types, mtime: this._configMtime }, { ops: { type: "in", mtime: "gt" }, pool: this.config }, function(err, rows) {
         if (err) return callback(err, []);
 
         // Sort inside to be persistent across databases
@@ -389,6 +395,7 @@ db.initConfig = function(options, callback)
             args[x.name] = 1;
             argv.push('-' + x.name);
             if (x.value) argv.push(x.value);
+            self._configMtime = Math.max(self._configMtime, x.mtime);
         });
         core.parseArgs(argv);
 
