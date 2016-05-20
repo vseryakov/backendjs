a0ddd63edd785d01a4f35a2f60bb0c596c79760a
Vlad Seryakov <vseryakov@gmail.com>
2016-05-19 22:32:34 -0400

    aws launch config

--- modules/bk_shell.js
+++ modules/bk_shell.js
@@ -1461,7 +1461,14 @@ shell.cmdAwsCreateLaunchConfig = function(options)
            if (!req.UserData) req.UserData = config.UserData;
            if (!req['BlockDeviceMappings.member.1.DeviceName']) {
                lib.objGet(config, "BlockDeviceMappings.member", { list: 1 }).forEach(function(x, i) {
-                   req["BlockDeviceMappings.member." + (i + 1)] = x;
+                   req["BlockDeviceMappings.member." + (i + 1) + ".DeviceName"] = x.DeviceName;
+                   if (x.VirtualName) req["BlockDeviceMappings.member." + (i + 1) + ".VirtualName"] = x.Ebs.VirtualName;
+                   if (x.Ebs && x.Ebs.VolumeSize) req["BlockDeviceMappings.member." + (i + 1) + ".Ebs.VolumeSize"] = x.Ebs.VolumeSize;
+                   if (x.Ebs && x.Ebs.VolumeType) req["BlockDeviceMappings.member." + (i + 1) + ".Ebs.VolumeType"] = x.Ebs.VolumeType;
+                   if (x.Ebs && x.Ebs.SnapshotId) req["BlockDeviceMappings.member." + (i + 1) + ".Ebs.SnapshotId"] = x.Ebs.SnapshotId;
+                   if (x.Ebs && x.Ebs.Iops) req["BlockDeviceMappings.member." + (i + 1) + ".Ebs.Iops"] = x.Ebs.Iops;
+                   if (x.Ebs && x.Ebs.Encrypted) req["BlockDeviceMappings.member." + (i + 1) + ".Ebs.Encrypted"] = x.Ebs.Encrypted;
+                   if (x.Ebs && typeof x.Ebs.DeleteOnTermination == "boolean") req["BlockDeviceMappings.member." + (i + 1) + ".Ebs.DeleteOnTermination"] = x.Ebs.DeleteOnTermination;
                });
            }
            if (!req["SecurityGroups.member.1"]) {
