d5bad92253d7206587dfec0bc0015dfcf94bc980
Vlad Seryakov <vseryakov@gmail.com>
2019-03-29 17:17:48 -0400

    list api endpoints and commands

---
+++ tools/endpoints.js
@@ -0,0 +1,52 @@
+//
+//  Author: Vlad Seryakov vseryakov@gmail.com
+//  Jan 2018
+//
+
+var fs = require('fs')
+var path = require("path");
+
+var files = fs.readdirSync(".").filter(function(x) { return fs.statSync(x).isFile() && x.match(/\.js$/); });
+files = files.concat(fs.readdirSync("lib/").filter(function(x) { return fs.statSync("lib/" + x).isFile() && x.match(/\.js$/); }).map(function(x) { return "lib/" + x }));
+files = files.concat(fs.readdirSync("modules/").filter(function(x) { return fs.statSync("modules/" + x).isFile() && x.match(/\.js$/); }).map(function(x) { return "modules/" + x }));
+
+var text = "";
+
+files.forEach(function(file) {
+    var state;
+    var data = fs.readFileSync(file).toString().split("\n");
+    for (var i = 0; i < data.length; i++) {
+        var line = data[i];
+        if (!line) continue;
+
+        // express endpoint
+        var d = line.match(/^ +api.app.(all|get|post)/);
+        if (d) {
+            text += "\n" + path.basename(file, '.js') + "\n  " + line.replace(/(^[^/]+|, function.+)/g, "").replace(/\\\//g, "/") + "\n    ";
+            state = 1;
+            continue;
+        }
+        // switch
+        d = line.match(/^ +switch \((req.params|cmd)/);
+        if (d && state == 1) {
+            state = 2;
+            continue;
+        }
+        // case
+        d = line.match(/^ +case ["']/);
+        if (d && state == 2) {
+            text += line.replace(/case |['":]/g, "").trim() + " ";
+            continue;
+        }
+        // default, end of switch
+        d = line.match(/default:|= function/);
+        if (d && state) {
+            state = 0;
+            text += "\n";
+        }
+    }
+});
+
+console.log(text);
+process.exit(0);
+
