60e7243e8ba05830ddd9906ef3bd0c0701f44d74
Vlad Seryakov <vseryakov@gmail.com>
2020-06-09 17:48:50 -0400

    text convertion utils

--- web/js/bkjs-lib.js
+++ web/js/bkjs-lib.js
@@ -844,6 +844,72 @@ bkjs.textToXml = function(str)
     });
 }
 
+bkjs.htmlEntities = {
+    "quot": "\"", "amp": "&", "lt": "<", "gt": ">", "nbsp": "\u00a0",
+    "iexcl": "¡", "cent": "¢", "pound": "£", "curren": "¤", "yen": "¥",
+    "brvbar": "¦", "sect": "§", "uml": "¨", "copy": "©", "ordf": "ª", "laquo": "«",
+    "not": "¬", "shy": "\u00ad", "reg": "®", "macr": "¯", "deg": "°", "plusmn": "±",
+    "sup2": "²", "sup3": "³", "acute": "´", "micro": "µ", "para": "¶", "middot": "·",
+    "cedil": "¸", "sup1": "¹", "ordm": "º", "raquo": "»", "frac14": "¼", "frac12": "½",
+    "frac34": "¾", "iquest": "¿", "Agrave": "À", "Aacute": "Á", "Acirc": "Â", "Atilde": "Ã",
+    "Auml": "Ä", "Aring": "Å", "AElig": "Æ", "Ccedil": "Ç", "Egrave": "È", "Eacute": "É",
+    "Ecirc": "Ê", "Euml": "Ë", "Igrave": "Ì", "Iacute": "Í", "Icirc": "Î", "Iuml": "Ï",
+    "ETH": "Ð", "Ntilde": "Ñ", "Ograve": "Ò", "Oacute": "Ó", "Ocirc": "Ô", "Otilde": "Õ",
+    "Ouml": "Ö", "times": "×", "Oslash": "Ø", "Ugrave": "Ù", "Uacute": "Ú", "Ucirc": "Û",
+    "Uuml": "Ü", "Yacute": "Ý", "THORN": "Þ", "szlig": "ß", "agrave": "à", "aacute": "á",
+    "acirc": "â", "atilde": "ã", "auml": "ä", "aring": "å", "aelig": "æ", "ccedil": "ç",
+    "egrave": "è", "eacute": "é", "ecirc": "ê", "euml": "ë", "igrave": "ì", "iacute": "í",
+    "icirc": "î", "iuml": "ï", "eth": "ð", "ntilde": "ñ", "ograve": "ò", "oacute": "ó",
+    "ocirc": "ô", "otilde": "õ", "ouml": "ö", "divide": "÷", "oslash": "ø", "ugrave": "ù",
+    "uacute": "ú", "ucirc": "û", "uuml": "ü", "yacute": "ý", "thorn": "þ", "yuml": "ÿ",
+    "OElig": "Œ", "oelig": "œ", "Scaron": "Š", "scaron": "š", "Yuml": "Ÿ", "fnof": "ƒ",
+    "circ": "ˆ", "tilde": "˜", "Alpha": "Α", "Beta": "Β", "Gamma": "Γ", "Delta": "Δ",
+    "Epsilon": "Ε", "Zeta": "Ζ", "Eta": "Η", "Theta": "Θ", "Iota": "Ι", "Kappa": "Κ",
+    "Lambda": "Λ", "Mu": "Μ", "Nu": "Ν", "Xi": "Ξ", "Omicron": "Ο", "Pi": "Π", "Rho": "Ρ",
+    "Sigma": "Σ", "Tau": "Τ", "Upsilon": "Υ", "Phi": "Φ", "Chi": "Χ", "Psi": "Ψ", "Omega": "Ω",
+    "alpha": "α", "beta": "β", "gamma": "γ", "delta": "δ", "epsilon": "ε", "zeta": "ζ", "eta": "η",
+    "theta": "θ", "iota": "ι", "kappa": "κ", "lambda": "λ", "mu": "μ", "nu": "ν", "xi": "ξ",
+    "omicron": "ο", "pi": "π", "rho": "ρ", "sigmaf": "ς", "sigma": "σ", "tau": "τ",
+    "upsilon": "υ", "phi": "φ", "chi": "χ", "psi": "ψ", "omega": "ω", "thetasym": "ϑ",
+    "upsih": "ϒ", "piv": "ϖ", "ensp": " ", "emsp": " ", "thinsp": " ", "zwnj": "‌\u200c",
+    "zwj": "‍\u200d", "lrm": "‎\u200e", "rlm": "\u200f‏", "ndash": "–", "mdash": "—",
+    "lsquo": "‘", "rsquo": "’", "sbquo": "‚", "ldquo": "“", "rdquo": "”", "bdquo": "„",
+    "dagger": "†", "Dagger": "‡", "permil": "‰", "lsaquo": "‹", "rsaquo": "›", "bull": "•",
+    "hellip": "…", "prime": "′", "Prime": "″", "oline": "‾", "frasl": "⁄",
+    "weierp": "℘", "image": "ℑ", "real": "ℜ", "trade": "™", "alefsym": "ℵ", "larr": "←",
+    "uarr": "↑", "rarr": "→", "darr": "↓", "harr": "↔", "crarr": "↵", "lArr": "⇐", "uArr": "⇑",
+    "rArr": "⇒", "dArr": "⇓", "hArr": "⇔", "forall": "∀", "part": "∂", "exist": "∃", "empty": "∅",
+    "nabla": "∇", "isin": "∈", "notin": "∉", "ni": "∋", "prod": "∏", "sum": "∑", "minus": "−",
+    "lowast": "∗", "radic": "√", "prop": "∝", "infin": "∞", "ang": "∠", "and": "⊥", "or": "⊦",
+    "cap": "∩", "cup": "∪", "int": "∫", "there4": "∴", "sim": "∼", "cong": "≅", "asymp": "≈",
+    "ne": "≠", "equiv": "≡", "le": "≤", "ge": "≥", "sub": "⊂", "sup": "⊃", "nsub": "⊄",
+    "sube": "⊆", "supe": "⊇", "oplus": "⊕", "otimes": "⊗", "perp": "⊥", "sdot": "⋅", "lceil": "⌈",
+    "rceil": "⌉", "lfloor": "⌊", "rfloor": "⌋", "lang": "〈", "rang": "〉", "loz": "◊", "spades": "♠",
+    "clubs": "♣", "hearts": "♥", "diams": "♦",
+};
+
+bkjs.textToEntity = function(str)
+{
+    if (typeof str != "string") return "";
+    if (!this.textEntities) {
+        this.textEntities = {};
+        for (var p in this.htmlEntities) this.textEntities[this.htmlEntities[p]] = "&" + p + ";";
+    }
+    return str.replace(/([&<>'":])/g, function(_, n) {
+        return bkjs.textEntities[n] || n;
+    });
+}
+
+// Convert html entities into their original symbols
+bkjs.entityToText = function(str)
+{
+    if (typeof str != "string") return "";
+    return str.replace(/&(#?[a-zA-Z0-9]+);/g, function(_, n) {
+        if (n[0] === '#') return n.charAt(1) === 'x' ? String.fromCharCode(parseInt(n.substring(2), 16)) : String.fromCharCode(+n.substring(1));
+        return bkjs.htmlEntities[n.toLowerCase()] || "";
+    });
+}
+
 // Split string into array, ignore empty items,
 // - `sep` is an RegExp to use as a separator instead of default  pattern `[,\|]`,
 // - `options` is an object with the same properties as for the `toParams`, `datatype' will be used with
