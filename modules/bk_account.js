9f30838f24c01ffd18930218c06db39433a65045
Vlad Seryakov <vseryakov@gmail.com>
2020-11-12 15:30:29 -0500

    db table aliases, cleanup bk_account mod

--- modules/bk_account.js
+++ modules/bk_account.js
@@ -77,21 +77,7 @@ mod.configureAccountsAPI = function()
 
         switch (req.params[0]) {
         case "get":
-            if (!req.query.id || req.query.id == req.account.id) {
-                mod.getAccount(req, options, function(err, data, info) {
-                    api.sendJSON(req, err, data);
-                });
-
-            } else {
-                db.list("bk_account", req.query.id, options, function(err, data) {
-                    api.sendJSON(req, err, data);
-                });
-            }
-            break;
-
-        case "add":
-            options.cleanup = "";
-            mod.addAccount(req, options, function(err, data) {
+            mod.getAccount(req, options, function(err, data, info) {
                 api.sendJSON(req, err, data);
             });
             break;
@@ -108,54 +94,11 @@ mod.configureAccountsAPI = function()
             });
             break;
 
-        case "subscribe":
-            api.subscribe(req);
-            break;
-
         case "put/secret":
             req.query.id = req.account.id;
             req.query.login = req.account.login;
             api.setAccountSecret(req.query, options, function(err, data) {
                 api.sendJSON(req, err, data);
-            });
-            break;
-
-        case "get/icon":
-            if (!core.modules.bk_icon) return api.sendReply(res, 400, "invalid request");
-            if (!req.query.id) req.query.id = req.account.id;
-            req.query.prefix = 'account';
-            if (!req.query.type) req.query.type = '0';
-            options.cleanup = "bk_icon";
-            core.modules.bk_icon.send(req, options);
-            break;
-
-        case "select/icon":
-            if (!core.modules.bk_icon) return api.sendReply(res, 400, "invalid request");
-            if (!req.query.id) req.query.id = req.account.id;
-            req.query.prefix = "account";
-            options.cleanup = "bk_icon";
-            core.modules.bk_icon.select(req.query, function(err, rows) {
-                api.sendJSON(req, err, rows);
-            });
-            break;
-
-        case "put/icon":
-            if (!core.modules.bk_icon) return api.sendReply(res, 400, "invalid request");
-            req.query.prefix = 'account';
-            req.query.id = req.account.id;
-            if (!req.query.type) req.query.type = '0';
-            core.modules.bk_icon.upload(req, options, function(err, data) {
-                api.sendJSON(req, err, data);
-            });
-            break;
-
-        case "del/icon":
-            if (!core.modules.bk_icon) return api.sendReply(res, 400, "invalid request");
-            req.query.prefix = 'account';
-            req.query.id = req.account.id;
-            if (!req.query.type) req.query.type = '0';
-            core.modules.bk_icon.del(req.query, function(err, rows) {
-                api.sendJSON(req, err);
             });
             break;
 
@@ -180,96 +123,37 @@ mod.getAccount = function(req, options, callback)
 // Send Push notification to the account. The delivery is not guaranteed, if the message was queued for delivery, no errors will be returned.
 //
 // The options may contain the following:
-//  - account_id - REQUIRED, the account to who to send the notification
+//  - account - the account record where to send the notification
+//  - account_id - the id of the account where to send, the record will be retrieved
+//  - device_id - the device to send the message directly
 //  - msg - message text to send
-//  - badge - a badge number to be sent
-//  - allow - the account properties to check if notifications are enabled, it must be an object with properties in the account record and values to
-//    be a regexp, each value if starts with "!" means not equal, see `lib.isMatched`
-//  - skip - Array or an object with account ids which should be skipped, this is for mass sending in order to reuse the same options
-//  - enable - the account properties to check if a notification parameter must be set or removed, an account property must be a boolean and
-//    if false then a notification parameter is removed and if true a notification parameter is set to 1
-//  - logging - logging level about the notification send status, default is debug, can be any valid logger level, must be a string, not a number
-//  - service_id - name of the standard delivery service supported by the backend, it is be used instead of custom handler, one of the following: apple, google
-//  - app_id - the application specific device tokens should be used only or if none matched the default device tokens
-//  - device_id - the device to send the message to instesd of the device_id property fro the account record
 //
 // In addition the device_id can be saved in the format service://id where the service is one of the supported delivery services, this way the notification
 // system will pick the right delivery service depending on the device id, the default service is apple.
 //
 //  Example:
 //
-//       bk_account.notifyAccount({ account_id: "123", msg: "test", badge: 1, sound: 1, allow: { notifications0: 1, type: "user" }, enable: { sound: "sound0", badge: "badge0" } })
+//       bk_account.notifyAccount({ account_id: "123", msg: "test", badge: 1, sound: 1 } })
 //
 mod.notifyAccount = function(options, callback)
 {
-    if (!options || !options.account_id) {
-        return lib.tryCall(callback, { status: 500, message: "invalid account" });
-    }
-
-    // Skip this account
-    switch (lib.typeName(options.skip)) {
-    case "array":
-        if (options.skip.indexOf(options.account_id) > -1) return lib.tryCall(callback, { status: 400, message: "skipped", id: options.account_id });
-        break;
-    case "object":
-        if (options.skip[options.account_id]) return lib.tryCall(callback, { status: 400, message: "skipped", id: options.account_id });
-        break;
-    }
-
-    var account = options.account;
+    var device_id = options.device_id || options.account && options.account.device_id;
     lib.series([
-      function(next) {
-          if (account && account.id == options.account_id) return next();
-          db.get("bk_account", { id: options.account_id }, function(err, row) {
-              if (err || !row) return next(err || { status: 404, message: "account not found", id: options.account_id });
-              if (!row.device_id && !row.device_id) return next({ status: 404, message: "device not found", id: options.account_id });
-              account = row;
-              next();
-          });
-      },
-      function(next) {
-          if (!lib.isMatched(account, options.allow)) {
-              return next({ status: 401, message: "not allowed", id: options.account_id });
-          }
-          for (var p in options.enable) {
-              var c = options.enable[p];
-              if (typeof account[c] == "undefined") continue;
-              if (lib.toBool(account[c])) options[p] = 1; else delete options[p];
-          }
-          msg.send(options.device_id || account.device_id, options, function(err) {
-              logger.logger(err ? "error" : "debug", "notifyAccount:", err, options, account);
-              next(err);
-          });
-      },
-    ], callback);
-}
-
-// Return account details for the list of rows, `options.account_key` specified the column to use for the account id in the `rows`, or `id` will be used.
-// The result accounts are cleaned for public columns, all original properties from the `rows` are kept as is.
-// If options.existing is 1 then return only record with found accounts, all other records in the rows will be deleted
-mod.listAccount = function(rows, options, callback)
-{
-    if (typeof options == "function") callback = options, options = null;
-    if (!options) options = {};
-    if (!rows) return callback(null, []);
-    var key = options.account_key || "id";
-    var map = {};
-    if (!Array.isArray(rows)) rows = [ rows ];
-    rows.forEach(function(x) { if (!map[x[key]]) map[x[key]] = []; map[x[key]].push(x); });
-    db.list("bk_account", Object.keys(map).map(function(x) { return { id: x } }), options, function(err, list, info) {
-        if (err) return callback(err, []);
-
-        api.checkResultColumns("bk_account", list, options);
-        list.forEach(function(x) {
-            map[x.id].forEach(function(row) {
-                for (var p in x) if (!row[p]) row[p] = x[p];
-                if (options.existing) row._id = 1;
+        function(next) {
+            if (device_id || !options.account_id) return next();
+            db.get("bk_account", { id: options.account_id }, function(err, row) {
+                if (err || !row) return next(err || { status: 404, message: "account not found", id: options.account_id });
+                device_id = row.device_id;
+                next();
             });
-        });
-        // Remove rows without account info
-        if (options.existing) rows = rows.filter(function(x) { return x._id; }).map(function(x) { delete x._id; return x; });
-        callback(null, rows, info);
-    });
+        },
+        function(next) {
+            msg.send(device_id, options, function(err) {
+                logger.logger(err ? "error" : "debug", "notifyAccount:", err, options, device_id);
+                next(err);
+            });
+        },
+    ], callback);
 }
 
 // Register new account, used in /account/add API call, but the req does not have to be an Express request, it just
@@ -422,73 +306,6 @@ mod.deleteAccount = function(req, callback)
             if (!err) api.metrics.Counter('auth_del_0').inc();
             logger.info("deleteAccount:", req.account.id, req.options.keep, lib.toAge(started));
             callback(err);
-        });
-    });
-}
-
-// Rename account alias
-mod.renameAccount = function(req, callback)
-{
-    if (!req.account || !req.account.id || !req.account.name) return callback({ status: 400, message: "no id and name provided" });
-    if (!req.options) req.options = {};
-    if (!req.query) req.query = {};
-
-    db.get("bk_account", { id: req.account.id }, req.options, function(err, account) {
-        if (err || !account) return callback(err || { status: 404, message: "No account found" });
-
-        lib.series([
-          function(next) {
-              if (api.authTable == "bk_account") return next();
-              if (req.account.name == account.name) return next();
-              db.update(api.authTable, { login: account.login, name: req.account.name }, next);
-          },
-          function(next) {
-              if (req.account.name == account.name) return next();
-              db.update("bk_account", { id: req.account.id, name: req.account.name }, next);
-          },
-          function(next) {
-              core.runMethods("bkRenameAccount", { account: req.account, query: req.query, options: req.options }, function() { next() });
-          },
-        ], callback);
-    });
-}
-
-// Override OAuth account management
-mod.fetchAccount = function(query, options, callback)
-{
-    if (typeof options == "function") callback = options, options = null;
-    if (!options) options = {};
-    db.get(api.authTable, { login: query.login }, function(err, auth) {
-        if (err) return callback(err);
-
-        if (auth) {
-            mod.getAccount({ query: {}, account: auth }, options, function(err, row) {
-                if (!err) for (var p in row) auth[p] = row[p];
-                callback(err, auth);
-            });
-            return;
-        }
-
-        lib.series([
-            function(next) {
-                // Pretend to be an admin
-                mod.addAccount({ query: query, account: { type: "admin" } }, options, function(err, row) {
-                    if (row) auth = row;
-                    next(err);
-                });
-            },
-            function(next) {
-                if (!query.icon) return next();
-                core.httpGet(query.icon, { binary: 1 }, function(err, params) {
-                    if (err || !params.data.length) return next();
-                    api.saveIcon(params.data, auth.id, { prefix: "account", type: "0", width: options.width }, function(err) {
-                        if (err || !core.modules.bk_icon) return next();
-                        db.put("bk_icon", { id: auth.id, prefix: "account", type: "0" }, options, function(err, rows) { next() });
-                    });
-                });
-            },
-        ], function(err) {
-            callback(err, auth);
         });
     });
 }
