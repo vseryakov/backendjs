a06f4a89e98fc14dd84decc90f67943232ae2ec9
Vlad Seryakov <vseryakov@gmail.com>
2022-07-08 13:20:34 -0400

    bk_data permissions

--- modules/bk_data.js
+++ modules/bk_data.js
@@ -6,10 +6,14 @@
 const bkjs = require('backendjs');
 const db = bkjs.db;
 const api = bkjs.api;
+const lib = bkjs.lib;
 
 // Account management
-var mod = {
+const mod = {
     name: "bk_data",
+    args: [
+        { name: "perms", type: "map", maptype: "list", descr: "Tables and allowed operations, ex: -bk_data-perms bk_config:select;put" },
+    ],
     controls: {
         region: { type: "string" },
         pool: { type: "string" },
@@ -29,46 +33,44 @@ mod.configureWeb = function(options, callback)
 mod.configureDataAPI = function()
 {
     // Return table columns
-    api.app.all(/^\/data\/columns\/?([a-z_0-9]+)?$/, function(req, res) {
+    api.app.all(/^\/data\/(columns)\/?([a-z_0-9]+)?$/, function(req, res) {
+        if (mod.perms && !lib.isFlag(mod.perms[req.params[1] || "*"], req.params[0])) return res.status(403).send("not allowed");
         var options = api.getOptions(req);
-        if (req.params[0]) {
-            return res.json(db.getColumns(req.params[0], options));
+        if (req.params[1]) {
+            return res.json(db.getColumns(req.params[1], options));
         }
-        // Cache columns and return, only the current region
-        delete options.region;
-        db.cacheColumns(options, function() {
-            res.json(db.getPool().dbcolumns);
-        });
+        res.json(db.tables);
     });
 
     // Return table keys
-    api.app.all(/^\/data\/keys\/([a-z_0-9]+)$/, function(req, res) {
+    api.app.all(/^\/data\/(keys)\/([a-z_0-9]+)$/, function(req, res) {
+        if (mod.perms && !lib.isFlag(mod.perms[req.params[1]], req.params[0])) return res.status(403).send("not allowed");
         var options = api.getOptions(req);
-        res.json(db.getKeys(req.params[0], options));
+        res.json(db.getKeys(req.params[1], options));
     });
 
     // Basic operations on a table
     api.app.all(/^\/data\/(select|scan|search|list|get|add|put|update|del|incr|replace)\/([a-z_0-9]+)$/, function(req, res) {
-        // Table must exist
-        var dbcols = db.getColumns(req.params[1]);
-        if (!dbcols) return api.sendReply(res, "Unknown table");
+        if (mod.perms && !lib.isFlag(mod.perms[req.params[1]], req.params[0])) return res.status(403).send("not allowed");
 
         var options = api.getOptions(req);
         options.noscan = 0;
 
+        if (!db.getColumns(req.params[1], options)) return api.sendReply(res, 404, "Unknown table");
+
         switch (req.params[0]) {
         case "scan":
             var rows = [];
-            db.scan(req.params[1], req.query, options, function(row, next) {
+            db.scan(req.params[1], req.query, options, (row, next) => {
                 rows.push(row);
                 next();
-            },function(err) {
+            }, (err) => {
                 api.sendJSON(req, err, rows);
             });
             break;
 
         default:
-            db[req.params[0]](req.params[1], req.query, options, function(err, rows, info) {
+            db[req.params[0]](req.params[1], req.query, options, (err, rows, info) => {
                 switch (req.params[0]) {
                 case "select":
                 case "search":
