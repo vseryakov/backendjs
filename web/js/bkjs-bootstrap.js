f785b0c0794fd44a6a786248114b25e387af4f74
Vlad Seryakov <vseryakov@gmail.com>
2022-08-31 18:23:01 -0400

    sanitize alerts

--- web/js/bkjs-bootstrap.js
+++ web/js/bkjs-bootstrap.js
@@ -43,7 +43,9 @@ bkjs.showAlert = function(obj, type, text, options)
     var aid = "alert-" + bkjs._alertNum++;
     var html = '<div id=' + aid + ' class="alert alert-dismissible alert-' + type + ' show ' + (options.css || "") + '" role="alert">';
     if (options.icon) html += '<i class="fa fa-fw ' + options.icon + '"></i>';
-    html += bkjs.sanitizer.run(typeof text == "string" ? text : text && text.message ? text.message : JSON.stringify(text)).replace(/\n/g, "<br>");
+    html += bkjs.sanitizer.run(typeof text == "string" ? options.safe ? text : bkjs.textToEntity(text) :
+                               text && text.message ? options.safe ? text.message : bkjs.textToEntity(text.message) :
+                               JSON.stringify(text)).replace(/\n/g, "<br>");
     html += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
     html += "</div>";
     var element = options.element || ".alerts";
