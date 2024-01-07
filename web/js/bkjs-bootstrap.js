f7af08d631bb1aa947178a4e2dacfd1822709618
Vlad Seryakov <vseryakov@gmail.com>
2024-01-07 00:56:27 -0500

    show toast method

--- web/js/bkjs-bootstrap.js
+++ web/js/bkjs-bootstrap.js
@@ -35,38 +35,48 @@ bkjs.showLoading = function(op)
     }
 }
 
+bkjs.getAlertText = function(text, options)
+{
+    text = text?.message || text?.text || text?.msg || text;
+    text = bkjs.isS(text) ? options.safe ? text : bkjs.textToEntity(text) : bkjs.formatJSON(text, { preset: "compact" }).replace(/[<>]/g, "");
+    return bkjs.sanitizer.run(text).replace(/\n/g, "<br>");
+}
+
 bkjs.showAlert = function(obj, type, text, options)
 {
     if (bkjs.isS(obj)) options = text, text = type, type = obj, obj = $("body");
     if (!text) return;
-    if (!options) options = {};
-    if (!bkjs._alertNum) bkjs._alertNum = 0;
-    if (type == "error") type = "danger";
-    var aid = "alert-" + bkjs._alertNum++;
-    var html = '<div id=' + aid + ' class="alert alert-dismissible alert-' + type + ' show fade" role="alert">';
-    if (options.icon) html += '<i class="fa fa-fw ' + options.icon + '"></i>';
-    html += bkjs.sanitizer.run(bkjs.isS(text) ? options.safe ? text : bkjs.textToEntity(text) :
-                               text?.message ? options.safe ? text.message : bkjs.textToEntity(text.message) :
-                               JSON.stringify(text).replace(/[<>]/g, "")).replace(/\n/g, "<br>");
-    html += this.isBS4 ? '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' :
-                         '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
-    html += "</div>";
-    var element = options.element || ".alerts";
+    var o = Object.assign({}, options);
+    o.type = o.type == "error" ? "danger" : o.type || "info";
+
+    var element = o.element || ".alerts";
     if (!$(obj).find(element).length) obj = $("body");
     var alerts = $(obj).find(element);
     if (!alerts.length) return;
-    if (options.hide || alerts.css("display") == "none") {
+
+    var html = `
+    <div class="alert alert-dismissible alert-${o.type} show fade" role="alert">
+        ${o.icon ? `<i class="fa fa-fw ${o.icon}"></i>` : ""}
+        ${bkjs.getAlertText(text, o)}
+        <button type="button" class="${!bkjs.isBS4 ? "btn-" : ""}close" data-dismiss="alert" aria-label="Close">
+            ${bkjs.isBS4 ? '<span aria-hidden="true">&times;</span>' : ""}
+        </button>
+    </div>`;
+    if (o.hide || alerts.css("display") == "none") {
         alerts.attr("data-alert", "hide");
         alerts.show();
     }
-    if (options.css) alerts.addClass(options.css);
-    if (!options.append) alerts.empty();
-    alerts.append(html);
-    if (!options.dismiss) {
-        setTimeout(() => { $("#" + aid).alert('close') }, (options.delay || 3000) * (type == "danger" ? 3 : type == "warning" ? 3 : type == "info" ? 2 : 1));
+    if (o.css) alerts.addClass(o.css);
+    if (o.clear) alerts.empty();
+    var el = $(html);
+    alerts.append(el);
+    if (!o.dismiss) {
+        o.delay = (o.delay || 3000) * (type == "danger" || type == "warning" ? 3 : type == "info" ? 2 : 1);
+        setTimeout(() => { el.alert('close') }, o.delay);
     }
-    $(obj).find("#" + aid).on('closed.bs.alert', bkjs.cleanupAlerts.bind(bkjs, alerts, options));
-    if (options.scroll) alerts[0].scrollIntoView();
+    el.on('closed.bs.alert', bkjs.cleanupAlerts.bind(bkjs, alerts, o));
+    if (o.scroll) alerts[0].scrollIntoView();
+    return el;
 }
 
 bkjs.hideAlert = function(obj, options)
@@ -177,6 +187,51 @@ bkjs.showLogin = function(options, callback)
 bkjs.hideLogin = function()
 {
     $("#bkjs-login-modal").modal("hide");
+}
+
+bkjs.showToast = function(obj, type, text, options)
+{
+    if (bkjs.isS(obj)) options = text, text = type, type = obj, obj = null;
+    if (!text) return;
+    var o = Object.assign({ type: type == "error" ? "danger" : bkjs.isS(type) && type || "info", now: Date.now(), delay: 5000, role: "alert" }, options || {});
+    var t = o.type[0];
+    var delay = o.delay * (t == "d" || t == "w" ? 3 : t == "i" ? 2 : 1);
+    var icon = o.icon || t == "s" ? "fa-check-circle" : t == "d" ? "fa-exclamation-circle" : t == "w" ? "fa-exclamation-triangle": "fa-info-circle";
+    var html = `
+    <div class="toast fade show ${o.type} ${o.css || ""}" role="${o.role}" aria-live="polite" aria-atomic="true" data-bs-autohide="${!o.dismiss}" data-bs-delay="${delay}">
+        <div class="toast-header ${o.css_header || ""}">
+            <span class="fa fa-fw ${icon} me-2 text-${o.type}" aria-hidden="true"></span>
+            <strong class="me-auto toast-title">${o.title || bkjs.toTitle(type)}</strong>
+            <small class="timer" aria-hidden="true">${o.countdown ? Math.round(delay/1000)+"s" : !o.notimer ? "just now" : ""}</small>
+            <button type="button" class="btn-close ms-2" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body ${o.css_body || ""}">
+            ${bkjs.getAlertText(text, o)}
+        </div>
+    </div>`;
+    if (!obj) {
+        obj = $("body").find(".toast-container");
+        if (!obj.length) obj = $("<div></div>", { "aria-live": "polite" }).appendTo($("body"));
+        var pos = o.pos == "tl" ? "top-0 start-0" :
+                  o.pos == "tr" ? "top-0 end-0" :
+                  o.pos == "ml" ? "top-50 start-0  translate-middle-y" :
+                  o.pos == "mc" ? "top-50 start-50 translate-middle" :
+                  o.pos == "mr" ? "top-50 end-0 translate-middle-y" :
+                  o.pos == "bl" ? "bottom-0 start-0" :
+                  o.pos == "bc" ? "bottom-0 start-50 translate-middle-x" :
+                  o.pos == "br" ? "bottom-0 end-0" : "top-0 start-50 translate-middle-x";
+        obj[0].className = `toast-container position-fixed ${pos} p-3`;
+    }
+    if (o.clear) obj.empty();
+    var el = $(html);
+    $(el).toast("show");
+    obj.prepend(el);
+    var timer = !o.notimer ? setInterval(() => {
+        if (!$(el)[0].parentElement) return clearInterval(timer);
+        $(el).find(".timer").text(o.countdown ? Math.round((delay - (Date.now() - o.now))/1000) + "s" : Math.round((Date.now() - o.now)/1000) +"s ago");
+    }, o.countdown ? o.delay/2 : o.delay) : "";
+    el.on("hidden.bs.toast", () => { clearInterval(timer); el.remove() });
+    return el;
 }
 
 $(function() {
