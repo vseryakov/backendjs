88a38ff8f1e88e16c12415b092c730a86c2e0c72
Vlad Seryakov <vseryakov@gmail.com>
2025-12-14 17:19:52 -0500

    wip

--- tests/jwt.test.js
+++ tests/jwt.test.js
@@ -1,17 +1,17 @@
 
 const { describe, it } = require('node:test');
 const assert = require('node:assert/strict');
-const { lib } = require("../");
+const { JWT } = require("../");
 
 const secret = 'a-secret'
 
-describe("jwt tests", async () => {
+describe("JWT tests", async () => {
 
 it('Issuer (correct - string)', async () => {
     const tok =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MzMwNDY0MDAsImlzcyI6ImNvcnJlY3QtaXNzdWVyIn0.gF8S6M2QcfTTscgxeyihNk28JAOa8mfL1bXPb3_E3rk'
 
-    const rc = await lib.jwt.verify(tok, secret, { alg: "HS256", iss: 'correct-issuer' })
+    const rc = await JWT.verify(tok, secret, { alg: "HS256", iss: 'correct-issuer' })
 
     assert.partialDeepStrictEqual(rc, {
         payload: {
@@ -23,19 +23,19 @@ it('Issuer (correct - string)', async () => {
 it('Token Expired', async () => {
     const tok =
       'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzMwNDYxMDAsImV4cCI6MTYzMzA0NjQwMH0.H-OI1TWAbmK8RonvcpPaQcNvOKS9sxinEOsgKwjoiVo'
-    const { err } = await lib.jwt.verify(tok, secret)
+    const { err } = await JWT.verify(tok, secret)
     assert.match(err?.message, /expired/);
 })
 
 it('HS512 sign & verify & decode', async () => {
     var payload = { message: 'hello world' }
-    const tok = await lib.jwt.sign(payload, secret, "HS512")
+    const tok = await JWT.sign(payload, secret, "HS512")
     const expected =
       'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJtZXNzYWdlIjoiaGVsbG8gd29ybGQifQ.RqVLgExB_GXF1-9T-k4V4HjFmiuQKTEjVSiZd-YL0WERIlywZ7PfzAuTZSJU4gg8cscGamQa030cieEWrYcywg'
 
     assert.strictEqual(tok.token, expected)
 
-    const rc = await lib.jwt.verify(tok.token, secret)
+    const rc = await JWT.verify(tok.token, secret)
 
     assert.partialDeepStrictEqual(rc, {
         header: {
