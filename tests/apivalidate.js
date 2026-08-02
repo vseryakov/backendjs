f2c60b4fe5eae781cb8ea5eaa2b7bc9ebc3216ab
Vlad Seryakov <vseryakov@gmail.com>
2026-08-02 14:16:45 -0400

    validate middleware tests

--- tests/apivalidate.js
+++ tests/apivalidate.js
@@ -12,10 +12,20 @@ describe('API validate tests', async () => {
             context.send(200, "ok")
         })
         api.app.all("/client/*", (context) => {
-            context.send(200, "ok")
+            context.send(200, typeof context.query.clientId)
         })
         api.app.all("/login", (context) => {
             context.send(200, "ok")
+        })
+        api.app.all("/session/:id", (context) => {
+            context.send(200, typeof context.params.id)
+        })
+        api.app.all("/user/*", (context) => {
+            context.send(200, typeof context.params[0])
+        })
+        api.app.use("#0", "/user/1", (context, next) => {
+            context.user = { id: 1, name: "test" };
+            next()
         })
     });
 
@@ -31,10 +41,22 @@ describe('API validate tests', async () => {
             { get: "/account/1" },
             { get: "/client/1", status: 400 },
             { get: "/client/2?clientId=aaa", status: 400 },
-            { get: "/client/3?clientId=123" },
+            { get: "/client/3?clientId=123", regexp: /number/ },
             { get: "/client/3?clientId=123", status: 429 },
             { get: "/login" },
             { url: "/login", status: 400 },
+            { url: "/user/1", status: 400 },
+            { url: "/user/1?id=1" },
+            { url: "/user/1?id=1", status: 429 },
+            { url: "/user/2?id=2" },
+            { url: "/user/2?id=2", status: 429 },
+            { url: "/session/1", user: token },
+            { url: "/session/2" },
+            { url: "/session/1", status: 429, user: token },
+            { put: "/session/2" },
+            { put: "/session/2" },
+            { put: "/session/2" },
+            { put: "/session/a" },
         ];
 
         await acheckAccess({ config });
